3cd40d57c0b4d120459640b7c5828f8403ae61dd
neha <jainn.ne@gmail.com>
2021-08-03 15:41:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import allReducer from './reducer';
+import Home from './component/Home'
+import SinglePost from './component/SinglePost'
+import AddPost from './component/AddPost'
+import EditPost from './component/EditPost'
+import { initialState } from './reducer/post';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+const store = createStore(allReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ Home }></Route>
+        <Route exact path="/add" component={ AddPost }></Route>
+        <Route exact path="/post/:postId" component={ SinglePost }></Route>
+        <Route exact path="/edit/:postId" component={ EditPost }></Route>
+      </Switch>
+    </Router>
+    </Provider>
   );
 }
 
